974dbea57ac458a9742986c786c8a6ebdf2d6599
ASXIIX <suchawadee414@gmail.com>
2024-06-23 14:21:46 +0700

    edit clolor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container mx-auto pt-4">
+    <div className="container p-4 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
       <div className="flex gap-4 justify-end pb-4">
         <div>
           <Link href="/" className="hover:text-blue-600">
